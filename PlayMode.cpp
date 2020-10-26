8e2fa07e9d59d902c592ee775dfc1438acbd80fe
friskydingo0 <paper.pavan@gmail.com>
2020-10-26 02:27:54 -0400

    Drawing an image based on an index value
    - Texture loading based on img-list that stores files
    - added 3 placeholder images for testing sake

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -4,17 +4,126 @@
 #include "gl_errors.hpp"
 #include "data_path.hpp"
 #include "hex_dump.hpp"
+#include "load_save_png.hpp"
 
 #include <glm/gtc/type_ptr.hpp>
-
+#include <fstream>
 #include <random>
 
 #define log std::cout
 
+std::vector< PlayMode::Texture > images;
+Load< std::string > image_str(LoadTagDefault, []() -> std::string const * {	// this load is totally fake. It's actually loading the pngs in the image list file. Couldn't figure out how to do it better
+	
+	std::string *ret = new std::string();
+	std::string img_name;
+	
+	glm::uvec2 size;
+	std::vector< glm::u8vec4 > data;
+	// load image for each line in the data file
+	std::ifstream img_data(data_path("img-list"));	// reading lines based on https://stackoverflow.com/a/7868998
+	while (img_data >> img_name)
+	{
+		log << "Loading " << img_name << std::endl;
+		load_png(data_path("images/"+img_name), &size, &data, LowerLeftOrigin);
+		images.emplace_back(PlayMode::Texture(size, data));
+	}
+	img_data.close();
+	return ret;
+});
+
 PlayMode::PlayMode(Client &client_) : client(client_) {
+	// from game0 base code
+	glGenBuffers(1, &vertex_buffer);
+	glGenBuffers(1, &element_buffer);
+
+	{ //vertex array mapping buffer for color_texture_program:
+		//ask OpenGL to fill vertex_buffer_for_color_texture_program with the name of an unused vertex array object:
+		glGenVertexArrays(1, &vertex_buffer_for_color_texture_program);
+
+		//set vertex_buffer_for_color_texture_program as the current vertex array object:
+		glBindVertexArray(vertex_buffer_for_color_texture_program);
+
+		//set vertex_buffer as the source of glVertexAttribPointer() commands:
+		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+
+		//set up the vertex array object to describe arrays of PongMode::Vertex:
+		glVertexAttribPointer(
+			color_texture_program.Position_vec4, //attribute
+			3, //size
+			GL_FLOAT, //type
+			GL_FALSE, //normalized
+			sizeof(Vertex), //stride
+			(GLbyte *)0 + 0 //offset
+		);
+		glEnableVertexAttribArray(color_texture_program.Position_vec4);
+		//[Note that it is okay to bind a vec3 input to a vec4 attribute -- the w component will be filled with 1.0 automatically]
+
+		glVertexAttribPointer(
+			color_texture_program.Color_vec4, //attribute
+			4, //size
+			GL_UNSIGNED_BYTE, //type
+			GL_TRUE, //normalized
+			sizeof(Vertex), //stride
+			(GLbyte *)0 + 4*3 //offset
+		);
+		glEnableVertexAttribArray(color_texture_program.Color_vec4);
+
+		glVertexAttribPointer(
+			color_texture_program.TexCoord_vec2, //attribute
+			2, //size
+			GL_FLOAT, //type
+			GL_FALSE, //normalized
+			sizeof(Vertex), //stride
+			(GLbyte *)0 + 4*3 + 4*1 //offset
+		);
+		glEnableVertexAttribArray(color_texture_program.TexCoord_vec2);
+
+		//done referring to vertex_buffer, so unbind it:
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		//done setting up vertex array object, so unbind it:
+		glBindVertexArray(0);
+
+		GL_ERRORS(); //PARANOIA: print out any OpenGL errors that may have happened
+	}
+
+	{ //assign texture:
+		
+		//ask OpenGL to fill white_tex with the name of an unused texture object:
+		glGenTextures(1, &png_tex);
+
+		//bind that texture object as a GL_TEXTURE_2D-type texture:
+		glBindTexture(GL_TEXTURE_2D, png_tex);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, images[0].size.x, images[0].size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, images[0].data.data());
+
+		//set filtering and wrapping parameters:
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		//since texture uses a mipmap and we haven't uploaded one, instruct opengl to make one for us:
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		//Okay, texture uploaded, can unbind it:
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+		GL_ERRORS(); //PARANOIA: print out any OpenGL errors that may have happened
+	}
 }
 
 PlayMode::~PlayMode() {
+	//----- free OpenGL resources -----
+	glDeleteBuffers(1, &vertex_buffer);
+	vertex_buffer = 0;
+
+	glDeleteVertexArrays(1, &vertex_buffer_for_color_texture_program);
+	vertex_buffer_for_color_texture_program = 0;
+
+	glDeleteTextures(1, &png_tex);
+	png_tex = 0;
 }
 
 bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size) {
@@ -26,11 +135,31 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			choice = 1;
 			is_dirty = true;
 			one.pressed = true;
+			glBindTexture(GL_TEXTURE_2D, png_tex);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, images[choice].size.x, images[choice].size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, images[choice].data.data());
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+			//since texture uses a mipmap and we haven't uploaded one, instruct opengl to make one for us:
+			glGenerateMipmap(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, 0);
 			return true;
 		} else if (evt.key.keysym.sym == SDLK_2) {
 			choice = 2;
 			is_dirty = true;
 			two.pressed = true;
+			glBindTexture(GL_TEXTURE_2D, png_tex);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, images[choice].size.x, images[choice].size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, images[choice].data.data());
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+			//since texture uses a mipmap and we haven't uploaded one, instruct opengl to make one for us:
+			glGenerateMipmap(GL_TEXTURE_2D);
+			glBindTexture(GL_TEXTURE_2D, 0);
 			return true;
 		} else if (evt.key.keysym.sym == SDLK_3) {
 			choice = 3;
@@ -70,7 +199,7 @@ void PlayMode::update(float elapsed) {
 	client.connections.back().send(is_dirty ? 'y' : 'n');
 
 	if (is_dirty) {
-		log << "Sending choice " << choice << std::endl;
+		log << "Sending choice " << std::to_string(choice) << std::endl;
 		client.connections.back().send(choice);
 	}
 
@@ -109,13 +238,84 @@ void PlayMode::update(float elapsed) {
 		}
 	}, 0.0);
 
-	choice = 0;
+	// choice = 0;
 	is_dirty = false;
 }
 
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
+
+	//---- compute vertices to draw ----
+
+	//vertices will be accumulated into this list and then uploaded+drawn at the end of this function:
+	std::vector< Vertex > vertices;
+	unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+	//inline helper function for rectangle drawing:
+	auto draw_rectangle = [&vertices](glm::vec2 const &center, glm::vec2 const &radius, glm::u8vec4 const &color) {
+		//draw rectangle as two CCW-oriented triangles:
+		vertices.emplace_back(glm::vec3(center.x-radius.x, center.y-radius.y, 0.0f), color, glm::vec2(0.0f, 0.0f));
+		vertices.emplace_back(glm::vec3(center.x+radius.x, center.y-radius.y, 0.0f), color, glm::vec2(1.0f, 0.0f));
+		vertices.emplace_back(glm::vec3(center.x+radius.x, center.y+radius.y, 0.0f), color, glm::vec2(1.0f, 1.0f));
+
+		vertices.emplace_back(glm::vec3(center.x-radius.x, center.y-radius.y, 0.0f), color, glm::vec2(0.0f, 0.0f));
+		vertices.emplace_back(glm::vec3(center.x+radius.x, center.y+radius.y, 0.0f), color, glm::vec2(1.0f, 1.0f));
+		vertices.emplace_back(glm::vec3(center.x-radius.x, center.y+radius.y, 0.0f), color, glm::vec2(0.0f, 1.0f));
+	};
+
+	draw_rectangle(glm::vec2(drawable_size.x * 0.5f, drawable_size.y * 0.5f), glm::vec2(100.0f, 100.0f), glm::u8vec4(0xff, 0xff, 0xff, 0xff));
+
+	
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+	
+	//use alpha blending:
+	// glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//don't use the depth test:
+	glDisable(GL_DEPTH_TEST);
+
+	//upload vertices to vertex_buffer:
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer); //set vertex_buffer as current
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), vertices.data(), GL_STATIC_DRAW); //upload vertices array
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//set color_texture_program as current program:
+	glUseProgram(color_texture_program.program);
+
+	glm::mat4 projection = glm::ortho(0.0f, (float)drawable_size.x, 0.0f, (float)drawable_size.y);
+	glUniformMatrix4fv(color_texture_program.OBJECT_TO_CLIP_mat4, 1, GL_FALSE, glm::value_ptr(projection));
+
+	//use the mapping vertex_buffer_for_color_texture_program to fetch vertex data:
+	glBindVertexArray(vertex_buffer_for_color_texture_program);
+
+	//bind the solid white texture to location zero so things will be drawn just with their colors:
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, png_tex);
+	
+	GL_ERRORS();
+	//run the OpenGL pipeline:
+	glDrawArrays(GL_TRIANGLES, 0, GLsizei(vertices.size()));
+
+	//unbind the solid white texture:
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	//reset vertex array to none:
+	glBindVertexArray(0);
+
+	//reset current program to none:
+	glUseProgram(0);
+	
+
+	GL_ERRORS(); //PARANOIA: print errors just in case we did something wrong.
+
+
+
+
+	// glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	// glClear(GL_COLOR_BUFFER_BIT);
 
 	{ //use DrawLines to overlay some text:
 		glDisable(GL_DEPTH_TEST);
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -1,6 +1,7 @@
 #include "Mode.hpp"
 
 #include "Connection.hpp"
+#include "ColorTextureProgram.hpp"
 
 #include <glm/glm.hpp>
 
@@ -17,6 +18,42 @@ struct PlayMode : Mode {
 	virtual void draw(glm::uvec2 const &drawable_size) override;
 
 	//----- game state -----
+
+	// loaded texture references
+	struct Texture {
+		Texture(glm::uvec2 const &size_, std::vector< glm::u8vec4 > const &data_) :
+			size(size_), data(data_) {}
+		glm::uvec2 size;
+		std::vector< glm::u8vec4 > data;
+	};
+	
+
+	// from game 0 base code
+	
+	//draw functions will work on vectors of vertices, defined as follows:
+	struct Vertex {
+		Vertex(glm::vec3 const &Position_, glm::u8vec4 const &Color_, glm::vec2 const &TexCoord_) :
+			Position(Position_), Color(Color_), TexCoord(TexCoord_) { }
+		glm::vec3 Position;
+		glm::u8vec4 Color;
+		glm::vec2 TexCoord;
+	};
+	static_assert(sizeof(Vertex) == 4*3 + 1*4 + 4*2, "PongMode::Vertex should be packed");
+	
+	//Shader program that draws transformed, vertices tinted with vertex colors:
+	ColorTextureProgram color_texture_program;
+
+	//Buffer used to hold vertex data during drawing:
+	GLuint vertex_buffer = 0;
+
+	//Buffer for elements
+	GLuint element_buffer = 0;
+
+	//Vertex Array Object that maps buffer locations to color_texture_program attribute locations:
+	GLuint vertex_buffer_for_color_texture_program = 0;
+
+	//Solid white texture:
+	GLuint png_tex = 0;
 
 	//input tracking:
 	struct Button {
